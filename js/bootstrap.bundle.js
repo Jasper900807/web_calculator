d8d06cd4f6a8df605444b06254c088f8b17255db
Jasper900807 <jasper900807@gmail.com>
2025-06-04 16:26:09 +0800

    矩陣計算機施工中

--- js/bootstrap.bundle.js
+++ js/bootstrap.bundle.js
@@ -2405,7 +2405,7 @@
     // it's not an issue. I don't think anyone ever specifies width on <html>
     // anyway.
     // Browsers where the left scrollbar doesn't cause an issue report `0` for
-    // this (e.g. Edge 2019, IE11, Safari)
+    // this (e.g. Edge 2024, IE11, Safari)
     return getBoundingClientRect(getDocumentElement(element)).left + getWindowScroll(element).scrollLeft;
   }
 
